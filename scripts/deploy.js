2d821d77689c50af13c9f3addad72d090a0a868e
nethra4321 <108627909+nethra4321@users.noreply.github.com>
2025-04-16 11:18:49 -0500

    setting up the dapp, hardhat, configuring, installing npm dependencies

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,10 @@ async function main() {
   const PiracyGuard = await hre.ethers.getContractFactory("PiracyGuard");
   const contract = await PiracyGuard.deploy(); // deploys to local node
 
-  await contract.waitForDeployment(); // ✅ CORRECT in Hardhat v2.20+
+  await contract.waitForDeployment(); 
   const address = await contract.getAddress();
 
-  console.log("✅ PiracyGuard deployed at:", address);
+  console.log("PiracyGuard deployed at:", address);
 }
 
 main().catch((err) => {
